6c7f7770351a3e0f241280a9c44281686b4ae8ab
SuryaS1729 <143543633+SuryaS1729@users.noreply.github.com>
2025-09-21 16:19:35 +0530

    ios production ready

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -127,13 +127,13 @@ export default function Root() {
         <Stack screenOptions={{ headerShown: false }}>
           <Stack.Screen name="index" options={{animation:'fade'}}/>
           <Stack.Screen name="onboarding/index" options={{ headerShown: false, animation:'fade' }} />
-          <Stack.Screen name="add" options={{  animation:'fade' }} />
-          <Stack.Screen name="history" options={{animation:"simple_push"}}/>
-          <Stack.Screen name="read" options={{animation:'ios_from_right'}} />
-          <Stack.Screen name="shloka/[id]" options={{animation:'fade'}}/>
+          <Stack.Screen name="add" options={{  animation:'fade',  animationDuration:100 }} />
+          <Stack.Screen name="history" options={{animation:"simple_push", animationDuration:300	}}/>
+          <Stack.Screen name="read" options={{animation:'slide_from_bottom', animationDuration:300}} />
+          <Stack.Screen name="shloka/[id]" options={{animation:'fade', animationDuration:100}}/>
           <Stack.Screen name="share" options={{animation:'slide_from_bottom'}}/>
-          <Stack.Screen name="bookmarks" options={{animation:'simple_push'}}/>
-          <Stack.Screen name="focus" options={{ title: 'Focus Mode' , animation:'fade'}} />
+          <Stack.Screen name="bookmarks" options={{animation:'fade',animationDuration:200}}/>
+          <Stack.Screen name="focus" options={{ title: 'Focus Mode' , animation:'fade',animationDuration:100}} />
 
         </Stack>
       </SQLiteProvider>
